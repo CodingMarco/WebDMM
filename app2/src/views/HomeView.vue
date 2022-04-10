9b1c7393c84a3867e71b72001ed7345a61002d8c
CodingMarco <codingmarco@gmail.com>
2022-04-10 21:21:39 +0200

    Not upgrading to WS

---
+++ app2/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p id="measurement_display">{{ voltage }}</p>
+  </div>
+</template>
+
+<script>
+//import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "HomeView",
+  data: function () {
+    return {
+      voltage: [],
+      connection: null,
+    };
+  },
+  created: function () {
+    console.log("Starting connection");
+    this.$socket.$subscribe("readout", (data) => {
+      console.log("Got data", data);
+      this.voltage = data;
+    });
+    console.log("Subscribed to readout");
+  },
+};
+</script>
+
+<style scoped>
+#measurement_display {
+  font-size: 100px;
+}
+</style>
